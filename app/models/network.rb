32bf531b9cd65f4ba6c0bef8989fa6d177ceaf65
jaime8793 <109973167+jaime8793@users.noreply.github.com>
2022-11-18 02:27:36 +0300

    Completed Assignment

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < ActiveRecord::Base
+    has_many :shows
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
+
+    def shows
+        self.shows.order(:name)
+    end
+
   
 end
